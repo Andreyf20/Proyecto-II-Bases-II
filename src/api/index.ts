31f698d00843cd5acd416d4921c7d06cd39ba4f2
Mauricio Quriós <mqbravo@gmail.com>
2020-08-17 18:30:06 -0600

    Added memsql price query

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,4 +1,4 @@
-import { env } from 'process';
+import { env, send } from 'process';
 import { Kafka } from 'kafkajs';
 import { mysql } from 'mysql';
 const express = require('express');
@@ -10,7 +10,7 @@ const kafka = new Kafka({
 })
 
 const memsqldb = mysql.createConnection({
-  host: 'localhost',
+  host: '25.97.193.57',
   user: 'root',
   password: '',
   database: 'Precio_Viajes'
@@ -18,7 +18,7 @@ const memsqldb = mysql.createConnection({
 
 memsqldb.connect(function (err) {
   if (err) throw err;
-  console.log("Connected!");
+  console.log("MemSQL Connected!");
 });
 
 
@@ -39,6 +39,40 @@ router.get('/', async (req, res) => {
   res.send('DONE')
 });
 
+router.get('/buscar/:origen/:destino/:fecha', async(req, res) => {
+  const origen = req.params.origen;
+  const destino = req.params.destino;
+  const fecha = req.params.fecha;
+
+  console.log(origen, destino, fecha)
+
+  if(!destino || !origen || !fecha) res.status(400).send({error : 'Debe especificar todos los parametros'})
+
+  // Send the data to hive through kafka to process the price
+  // TODO: n
+
+  // Send the request to get the price in memsql
+  executeSearchQuery(req, res, origen, destino, fecha)
+})
+
+function executeSearchQuery(req, res, origen, destino, fecha) {
+    const sql_statement = `select precio from precios where origen = '${origen}' and destino = '${destino}' and DATEDIFF(fecha, '${fecha}') = 0 order by id desc limit 1`;
+
+    memsqldb.query(sql_statement, (err, result) => {
+      if (err) {
+        console.log(err)
+        res.status(400).send({error : 'Ocurrió un error con la consulta'});
+      }
+      console.log("RESPUESTA: ", result);
+  
+      if(result.length < 1){
+        // Keep asking memsql for the price until it gets a response from hive
+        executeSearchQuery(req, res, origen, destino, fecha)
+      }else
+        res.status(200).send({"precio" : result[0].precio}) 
+    });
+}
+
 router.get('/consume', async (req, res) => {
   const consumer = kafka.consumer({ groupId: 'test-group' })
 
@@ -54,24 +88,5 @@ router.get('/consume', async (req, res) => {
   })
   res.send('DONE')
 });
-
-router.get('/precio', async (req, res) => {
-  var sql = 'SELECT * FROM precios ORDER BY origen';
-
-  memsqldb.query(sql, (err, result) => {
-    if (err) {
-      throw err;
-    }
-    if (!result || !result.length) {
-      console.log("No results")
-      return;
-    }
-    console.log(result);
-  });
-});
-
-
-
-
 
 export = router;
